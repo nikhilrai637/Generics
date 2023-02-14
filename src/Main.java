945eb35a56ba26178b7c08201f1eed351c61427a
Nikhil Rai <Nikhil.Rai@Polestarllp.local>
2023-02-14 21:54:20 +0530

    Multiple Type Parameters 1

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,7 @@ import generics.Utils;
 
 public class Main {
     public static void main(String[] args) {
-        var max = Utils.max(new User(10),new User(50));
-        System.out.println(max);
+        Utils.print(1,"One");
     }
 
 }
--- src/generics/Utils.java
+++ src/generics/Utils.java
@@ -4,4 +4,8 @@ public class Utils {
     public static <T extends Comparable<T>> T max(T a , T b){
         return  a.compareTo(b) <= 0 ? b : a;
     }
+
+    public static <K,V> void print(K key,V value){
+        System.out.println("Key = " + key + " value = "+ value);
+    }
 }
