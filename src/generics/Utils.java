f1b436b5f870ecc14645a25d66ce6365b1d0b3fa
Nikhil Rai <Nikhil.Rai@Polestarllp.local>
2023-02-15 00:07:16 +0530

    Reading from a wildcard generic list

--- src/generics/Utils.java
+++ src/generics/Utils.java
@@ -11,9 +11,16 @@ public class Utils {
     // CAP#1 {} extends User
     // Instructor extends User
 
-    public static void printList(GenericList<? extends User> userList){
-       User user =  userList.get(0);
-       Instructors user = userList.get(0); // CAP#1 extends user is not a subclass of Instructors  but of User
-       userList.add(Cap) // np such capture class
+    public static void printList(GenericList<? super User> userList){
+      // User user =  userList.get(0);
+      // Instructors user = userList.get(0); // CAP#1 extends user is not a subclass of Instructors  but of User
+      // userList.add(Cap) // np such capture class
+
+
+        //Here  user and all its subclasses
+        userList.add(new User(0)); //
+        userList.add(new Instructors(10));
+
+
     }
 }
